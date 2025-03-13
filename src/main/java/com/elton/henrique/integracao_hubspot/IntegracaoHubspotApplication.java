9a7ff1878a93c53f7c1989d2634637b8dc2db62e
Elton-Henrique-Oliveira <terezafe.oli@gmail.com>
2025-03-12 21:28:07 -0300

    Projeto inicial

---
+++ src/main/java/com/elton/henrique/integracao_hubspot/IntegracaoHubspotApplication.java
@@ -0,0 +1,13 @@
+package com.elton.henrique.integracao_hubspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracaoHubspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracaoHubspotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elton/henrique/integracao_hubspot/IntegracaoHubspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elton.henrique.integracao_hubspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracaoHubspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
